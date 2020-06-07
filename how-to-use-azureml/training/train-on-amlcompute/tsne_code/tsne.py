3bf76c8fa7e9920c267965b7fbf534ee9cb2f378
Anders Swanson <swanson.anders@gmail.com>
2020-06-06 17:54:00 -0700

    pivot to so question

--- how-to-use-azureml/training/train-on-amlcompute/tsne_code/tsne.py
+++ how-to-use-azureml/training/train-on-amlcompute/tsne_code/tsne.py
@@ -1,18 +1,21 @@
-# Author: Jake Vanderplas -- <vanderplas@astro.washington.edu>
-
-from collections import OrderedDict
-from functools import partial
-from time import time
-
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import NullFormatter
+from collections import OrderedDict
 
+from time import time
+
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 from sklearn import manifold, datasets
+from sklearn.manifold import TSNE
 
-from azureml.core import VERSION as amlversion
-from azureml.core import Run
+
+from azureml.core import Run, VERSION as amlversion
+
 from gensim import __version__ as gsversion
 from sklearn import __version__ as skversion
 from scipy import __version__ as spversion
@@ -26,12 +29,9 @@ print(f"scipy version: {spversion}")
 print(f"numpy version: {npversion}")
 print(f"pandas version: {pdversion}")
 
-
-# Next line to silence pyflakes. This import is needed.
-Axes3D
-
+random_state = 42
 n_points = 1000
-X, color = datasets.make_s_curve(n_points, random_state=0)
+X, color = datasets.make_s_curve(n_points, random_state=random_state)
 n_neighbors = 10
 n_components = 2
 
@@ -45,37 +45,28 @@ ax = fig.add_subplot(251, projection='3d')
 ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color, cmap=plt.cm.Spectral)
 ax.view_init(4, -72)
 
-# Set-up manifold methods
-LLE = partial(manifold.LocallyLinearEmbedding,
-              n_neighbors, n_components, eigen_solver='auto')
 
 methods = OrderedDict()
-# methods['LLE'] = LLE(method='standard')
-# methods['LTSA'] = LLE(method='ltsa')
-# methods['Hessian LLE'] = LLE(method='hessian')
-# methods['Modified LLE'] = LLE(method='modified')
-# methods['Isomap'] = manifold.Isomap(n_neighbors, n_components)
-# methods['MDS'] = manifold.MDS(n_components, max_iter=100, n_init=1)
-# methods['SE'] = manifold.SpectralEmbedding(n_components=n_components,
-#                                            n_neighbors=n_neighbors)
+methods['PCA'] = PCA(n_components=n_components, random_state = 42)
 methods['t-SNE'] = manifold.TSNE(n_components=n_components, init='pca',
-                                 random_state=0)
+                                 verbose=1, perplexity=40, n_iter=300, 
+                                 random_state=42)
 
-Y = methods['t-SNE'].fit_transform(X)
-
-df_subset = pd.DataFrame()
-
-df_subset['tsne-2d-one'] = tsne_results[:,0]
-df_subset['tsne-2d-two'] = tsne_results[:,1]
-
-print(df_subset.head())
 
 run = Run.get_context()
 
+results = {}
 # Plot results
 for i, (label, method) in enumerate(methods.items()):
     t0 = time()
     Y = method.fit_transform(X)
+    if label == 't-SNE':
+        run.log("kl_divergence", method.kl_divergence_)
+    elif label == 'PCA':
+        run.log_list("PCA means", method.mean_)
+    results['{}_two'.format(label)] = Y[:1][0]
+    run.log('{}_two_one'.format(label), Y[:1][0][0])
+    run.log('{}_two_two'.format(label), Y[:1][0][1])
     t1 = time()
     run.log(label, "%.2g sec" % (t1 - t0))
     ax = fig.add_subplot(2, 5, 2 + i + (i > 3))
@@ -86,5 +77,9 @@ for i, (label, method) in enumerate(methods.items()):
     ax.axis('tight')
 
 
-
+plt.savefig('outputs/plot.png')
 run.log_image(name="results", plot=plt)
+
+df_results = pd.DataFrame(results)
+print(df_results.head())
+
