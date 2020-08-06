38d860e4c4fad56630f3f6d63408dab4169f5b06
Anders Swanson <swanson.anders@gmail.com>
2020-08-05 17:55:48 -0700

    reprex!

---
+++ how-to-use-azureml/training/train-on-amlcompute/train/train.py
@@ -0,0 +1,9 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license.
+
+import os
+
+os.makedirs('./outputs', exist_ok=True)
+
+os.makedirs('c:\\\\temp\\output', exist_ok=True)
+
